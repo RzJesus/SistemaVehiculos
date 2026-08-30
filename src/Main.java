55c18b3b9cb6f91f9ec80f0f1ab4af64f4420255
Jesus Ruiz <jesusgabrielzr03@gmail.com>
2026-08-29 23:26:36 -0500

    "Cree un vehiculo usando constructor con parametros"

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,16 @@ public class Main {
     public static void main(String[] args){
         Vehiculo vehiculo1 = new Vehiculo();
 
+        Vehiculo vehiculo2 = new Vehiculo("Toyota",2025);
+
         System.out.println(vehiculo1.marca);
         System.out.println(vehiculo1.anioFabricaion);
         System.out.println(vehiculo1.precioBase);
         System.out.println(vehiculo1.importado);
+
+        System.out.println(vehiculo2.marca);
+        System.out.println(vehiculo2.anioFabricaion);
+        System.out.println(vehiculo2.precioBase);
+        System.out.println(vehiculo2.importado);
     }
 }

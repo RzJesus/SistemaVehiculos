d552eac75da1283e9e0eca8dc734f83302b12170
Jesus Ruiz <jesusgabrielzr03@gmail.com>
2026-08-29 23:45:28 -0500

    "Agregue constructor a Carro.java"

--- src/Carro.java
+++ src/Carro.java
@@ -2,6 +2,14 @@ public class Carro extends Vehiculo {
     int numeroPuertas;
     String tipoCombustible;
 
+    public Carro( String marca, int anioFabricacion, int numeroPuertas, String tipoCombustible){
+
+        super (marca , anioFabricaion);
+        this.numeroPuertas = numeroPuertas;
+        this.tipoCombustible = tipoCombustible;
+
+    }
+
 
 
 }
